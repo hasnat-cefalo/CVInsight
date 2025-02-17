76a49b0192f9118432a58d23bbdde7677b345922
hasnat <abdullah.2010bd@gmail.com>
2025-02-17 17:07:33 +0600

    modified ollama_service.py to get host value from settings.

--- app/services/cv_processor_services/ollama_service.py
+++ app/services/cv_processor_services/ollama_service.py
@@ -1,9 +1,8 @@
 import json
-from typing import Optional
 
 from ollama import Client
 
-# from app.config import settings
+from app.config import settings
 from app.models.cv_model import CVModel
 from app.services.cv_processor_services.base_service import BaseService
 from app.utils.prompt import prompt
@@ -13,7 +12,7 @@ class OllamaService(BaseService):
     def __init__(self, model: str):
         self.model = model
         self.client = Client(
-            host="localhost:11434",
+            host=settings.ollama_host,
         )
 
     def _call_api(self, text: str) -> dict:
